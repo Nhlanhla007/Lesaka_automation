b9188d16ffb0da2b71fecf808662ed146193d089
thoko <tshabalala@shoprite.co.za>
2021-03-30 18:37:49 +0200

    removing duplicate Sap class

--- src/test/java/tests/JDTests.java
+++ src/test/java/tests/JDTests.java
@@ -169,7 +169,6 @@ public class JDTests extends BaseTest {
 		IC_CreditAppAddressDetails creditAppAddressDetails = new IC_CreditAppAddressDetails(driver, dataTable2);
 		ic_SubscriberNewsletter_DuplicateEmailaddress ic_SubscribeNews_DupliEmailID = new ic_SubscriberNewsletter_DuplicateEmailaddress(driver, dataTable2);
 		ic_newLetterInvalidEmail icNewsletterEmail = new ic_newLetterInvalidEmail(driver, dataTable2);
-		SAPCustomerRelated customerDB = new SAPCustomerRelated(driver,dataMap2);
 		IC_ProductsSortBy productsSortBy = new IC_ProductsSortBy(driver, dataTable2);
 		ExtentTest test1=test.createNode(moduleToRun);
 		int rowNumber=-1;
