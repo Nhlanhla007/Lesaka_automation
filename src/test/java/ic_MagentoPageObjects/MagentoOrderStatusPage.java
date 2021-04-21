17e372168061619ec29041d2170fadf92c83331a
thoko <tshabalala@shoprite.co.za>
2021-04-21 09:33:13 +0200

    updated navigateToOrderPage to remove try catch

--- src/test/java/ic_MagentoPageObjects/MagentoOrderStatusPage.java
+++ src/test/java/ic_MagentoPageObjects/MagentoOrderStatusPage.java
@@ -84,32 +84,28 @@ public class MagentoOrderStatusPage {
 		magentoTableRecords.get(0);
 	}
 	
-	public void searchForOrder(String idToSearch, ExtentTest test) {
-		try {
-			if (action.isDisplayed(clearFilters)) {
-				Thread.sleep(5000);
-				action.click(clearFilters, "Cleared Filters", test);
-				Thread.sleep(5000);
-			}
-			action.click(magentoFilterTab, "Filter tab", test);
-			action.writeText(magentoIdSearchField, idToSearch, "searchId", test);
-			action.click(magentoApplyFilterTab, "Apply to filters", test);
-			// verify if a row is returned
-			Thread.sleep(20000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+	public void searchForOrder(String idToSearch, ExtentTest test) throws InterruptedException, IOException {
+		if (action.isDisplayed(clearFilters)) {
+			Thread.sleep(5000);
+			action.click(clearFilters, "Cleared Filters", test);
+			Thread.sleep(5000);
+		}
+		action.click(magentoFilterTab, "Filter tab", test);
+		action.writeText(magentoIdSearchField, idToSearch, "searchId", test);
+		action.click(magentoApplyFilterTab, "Apply to filters", test);
+		// verify if a row is returned
+		Thread.sleep(20000);
 	}
 	
 	public void viewOrderDetails(ExtentTest test) throws IOException, InterruptedException {
-			confirmRows(magentoTableRecords, test);
-			if (magentoTableRecords.size() == 1) {
-				action.click(viewOrderDetails, "Order Status", test);
-				Thread.sleep(10000);
-				action.checkIfPageIsLoadedByURL("sales/order/view/order_id/", "View Details Page", test);
-			} else {
-				action.checkIfPageIsLoadedByURL("sales/order/view/order_id/", "View Details Page", test);
-			}
+		confirmRows(magentoTableRecords, test);
+		if (magentoTableRecords.size() == 1) {
+			action.click(viewOrderDetails, "Order Status", test);
+			Thread.sleep(10000);
+			action.checkIfPageIsLoadedByURL("sales/order/view/order_id/", "View Details Page", test);
+		} else {
+			action.checkIfPageIsLoadedByURL("sales/order/view/order_id/", "View Details Page", test);
+		}
 	}
 	
 	public void orderStatusCheck(String orderStatus, ExtentTest test) throws IOException {
@@ -117,7 +113,7 @@ public class MagentoOrderStatusPage {
 	}
 	
 	@Step("Navigates to the order page")
-	public void navigateToOrderPage(HashMap<String, ArrayList<String>> input, ExtentTest test, int rowNumber) {
+	public void navigateToOrderPage(HashMap<String, ArrayList<String>> input, ExtentTest test, int rowNumber) throws IOException, InterruptedException {
 //		String POfetchFrom = dataTable2.getValueOnOtherModule("OrderStatusSearch", "Fetch PO number", 0);
 //		String idToSearch = dataTable2.getValueOnOtherModule("PayUPagePayment","OrderID",0);
 		String idToSearch = dataTable2.getValueOnOtherModule("ic_RetriveOrderID","orderID",0);
@@ -128,16 +124,11 @@ public class MagentoOrderStatusPage {
 //		}
 		String orderStatus = input.get("orderStatus").get(rowNumber);
 		System.out.println("orderStatus :"+orderStatus);
-		try {
-			action.explicitWait(15000);
-			NavigateOdersPage(test);
-			searchForOrder(idToSearch,test);
-			orderStatusCheck(orderStatus, test);
-			viewOrderDetails(test);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		action.explicitWait(15000);
+		NavigateOdersPage(test);
+		searchForOrder(idToSearch,test);
+		orderStatusCheck(orderStatus, test);
+		viewOrderDetails(test);
 	}
 	public void NavigateOdersPage(ExtentTest test) throws IOException, InterruptedException{
 		action.click(magentoSalesTab, "Sales tab", test);
