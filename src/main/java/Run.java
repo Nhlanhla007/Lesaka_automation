1bece65e9fc924cc8cb532b48419f28d6de69dc4
thoko <tshabalala@shoprite.co.za>
2021-04-28 16:12:35 +0200

    adding endtoend _tz changes

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -10,7 +10,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -54,20 +54,25 @@ public class Run {
         classes.appendChild(className);
         Element methods = document.createElement("methods");
         Attr classNameAttr = document.createAttribute("name");
-        classNameAttr.setValue("tests.JDGTest_TestNG");
+//        classNameAttr.setValue("tests.JDGTest_TestNG");
+        classNameAttr.setValue("tests.testTestClass");
         className.setAttributeNode(classNameAttr);
         className.appendChild(methods);
+        String sampleTestMethod=FileToString("src/test/resources/sampleTestMethod");
+        String sampleTestClass=FileToString("src/test/resources/sampleTestClass");
+        String allTestMethods="";
         for(int i=0;i<numberOfRows;i++){
             if(suiteIC.get("Execute").get(i).toLowerCase().equals("yes")){
                 Element include = document.createElement("include");
                 Attr includeName = document.createAttribute("name");
                 includeName.setValue(suiteIC.get("Test_Case_Name").get(i));
+                allTestMethods=allTestMethods+System.getProperty("line.separator")+sampleTestMethod.replaceAll("\\$\\{testCaseName}",suiteIC.get("Test_Case_Name").get(i)).replace("${TCID}",suiteIC.get("TestCaseID").get(i));
                 include.setAttributeNode(includeName);
                 methods.appendChild(include);
-//                System.out.println("include");
-//                System.out.println(suiteIC.get("Test Case Name").get(i));
             }
         }
+        sampleTestClass=sampleTestClass.replace("${AllTestMethods}",allTestMethods);
+        stringToFile("src/test/java/tests/testTestClass.java",sampleTestClass);
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -76,6 +81,38 @@ public class Run {
         StreamResult streamResult = new StreamResult(new File("testNG.xml"));
         transformer.transform(domSource, streamResult);
         System.out.println("Done creating XML File");
+    }
+    public static String FileToString(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        String ls = System.getProperty("line.separator");
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+// delete the last new line separator
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        reader.close();
+        String content = stringBuilder.toString();
+        return content;
+    }
+    public static void stringToFile(String fileName,String content){
+        try {
+            File myObj = new File(fileName);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter(myObj);
+            myWriter.write(content);
+            myWriter.close();
+//            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 
