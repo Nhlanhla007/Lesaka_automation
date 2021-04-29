2894c9f26d97ca8a4b67ff6c05cd606a78b5aff2
thoko <tshabalala@shoprite.co.za>
2021-04-29 15:31:24 +0200

    latest XL sheet included

--- src/test/java/tests/JDGTest_TestNG.java
+++ src/test/java/tests/JDGTest_TestNG.java
@@ -6,11 +6,15 @@ import SAP_HanaDB.SAPorderRelated;
 import base.TestCaseBase;
 import com.aventstack.extentreports.ExtentTest;
 import emailverification.ICGiftCardVerification;
+import emailverification.ICWishlistverification;
+import emailverification.ic_PasswordForgotEmailVerification;
+import emailverification.ic_ResetPasswordEmailLink;
 import ic_MagentoPageObjects.*;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 import utils.*;
 import Logger.Log;
@@ -149,13 +153,12 @@ public class JDGTest_TestNG<moduleName> {
         }
     }
 
-    public void runKeyWord(String actionToRun, ExtentTest test) throws Exception {
+    public void runKeyWord(String actionToRun,ExtentTest test) throws Exception {
         String moduleToRun=actionToRun;
-        ExtentTest test1=test.createNode(moduleToRun);
         IConnection ic=new IConnection(driver,dataTable2);
         Magento_UserInfoVerification Magentoverify = new Magento_UserInfoVerification(driver,dataTable2);
         ic_PaymentOption Payopt=new ic_PaymentOption(driver,dataTable2);
-        ic_PayUPayment PayU = new ic_PayUPayment(driver,dataTable2);
+        ic_PayUPayment  PayU = new ic_PayUPayment(driver,dataTable2);
         Ic_Products products = new Ic_Products(driver,dataTable2);
         IC_Cart icCart=new IC_Cart(driver,dataTable2);
         ic_AccountConfirmation icAccountConfirmation = new ic_AccountConfirmation(driver,dataTable2);
@@ -173,6 +176,13 @@ public class JDGTest_TestNG<moduleName> {
         admin_UserUpdate adminUserUpdate = new admin_UserUpdate(driver,dataTable2);
         customerValidationUpdates customerVerifyEdits = new customerValidationUpdates(driver,dataTable2);
         ic_Login ic_login = new ic_Login(driver,dataTable2);
+        ic_verifyDeliveryOptions icDeliveryOptionDisplay = new ic_verifyDeliveryOptions(driver,dataTable2);
+        ic_invalidLoginCreds ic_invalidCredslogin = new ic_invalidLoginCreds(driver, dataTable2);
+        ic_LoginPasswordIsSecured icPasswordSecured = new ic_LoginPasswordIsSecured(driver, dataTable2);
+        ic_forgotPasswordLink icforgottenPassLink = new ic_forgotPasswordLink(driver, dataTable2);
+        verifyForgotPassword icVerifyForgotPass = new verifyForgotPassword(driver, dataTable2);
+        ic_PasswordForgotEmailVerification icForgotEmailSent = new ic_PasswordForgotEmailVerification(driver, dataTable2);
+        ic_ResetPasswordEmailLink ResetPasswordLink = new ic_ResetPasswordEmailLink(driver, dataTable2);
         ic_CashDepositPayment ic_cashDepositPayment =new ic_CashDepositPayment(driver,dataTable2);
         SAPorderRelated SaporderRelated = new SAPorderRelated(driver,dataMap2,dataTable2);
         ICGiftCardVerification icGiftCardVerification = new ICGiftCardVerification(driver,dataTable2);
@@ -198,17 +208,52 @@ public class JDGTest_TestNG<moduleName> {
         ic_SubscriberNewsletter_DuplicateEmailaddress ic_SubscribeNews_DupliEmailID = new ic_SubscriberNewsletter_DuplicateEmailaddress(driver, dataTable2);
         ic_newLetterInvalidEmail icNewsletterEmail = new ic_newLetterInvalidEmail(driver, dataTable2);
         IC_ProductsSortBy productsSortBy = new IC_ProductsSortBy(driver, dataTable2);
+        ic_WishlistToCart IC_WishlistToCart =new ic_WishlistToCart(driver, dataTable2);
+        ic_verifyWishlistItem verifyWishlistItem = new ic_verifyWishlistItem(driver, dataTable2);
+        ic_RemoveFromcart RemoveFromcart = new ic_RemoveFromcart(driver, dataTable2);
+        ic_WishList WishList = new ic_WishList(driver, dataTable2);
+        ic_CompareProducts productsCompared = new ic_CompareProducts(driver, dataTable2);
+        ic_NavigetoWishlist NavigetoWishlist = new ic_NavigetoWishlist(driver, dataTable2);
+        IC_verifyLogin ic_verifyLogin =new IC_verifyLogin(driver, dataTable2);
+        IC_IncreaseQuanityInCart increQuantity = new IC_IncreaseQuanityInCart(driver, dataTable2);
+        IC_RemoveItemsFromCart removeItemsFromCart = new IC_RemoveItemsFromCart(driver, dataTable2);
+        ic_SendWishlistToEmail SendWishlistToEmail = new ic_SendWishlistToEmail(driver, dataTable2);
+        ICWishlistverification icEmailWishlistverification = new ICWishlistverification(driver, dataTable2);
+        RedirectToProdDetailPageFromCart redirectAndVerify = new RedirectToProdDetailPageFromCart(driver, dataTable2);
+        IC_Pagination pagination = new IC_Pagination(driver, dataTable2);
+        ExtentTest test1=test.createNode(moduleToRun);
         int rowNumber=-1;
         if(dataMap2.containsKey(currentKeyWord+"++")) {
             rowNumber = findRowToRun(dataMap2.get(currentKeyWord + "++"), occCount.get(currentKeyWord), testcaseID);
         }
         int i = 0;
+        WebElement el = null;
         switch (moduleToRun) {
             case "Login":
                 ic.login(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
                 break;
             case "ic_login":
                 ic_login.Login_ic(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
+                break;
+            case "ic_invalidCredslogin":
+                ic_invalidCredslogin.invalidLogin_ic(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "icPasswordSecured":
+                icPasswordSecured.loginPasswordSafe(dataMap2.get("ic_login++"), test1, rowNumber);
+                break;
+            case "icForgotPasswordLink":
+                icforgottenPassLink.forgotPasswordLink(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
+                break;
+            case "icVerifyForgotPass":
+                icVerifyForgotPass.forgotPasswordPage(dataMap2.get("accountCreation++"),test1,rowNumber);
+                break;
+            case "icEmailSentVerification":
+                icForgotEmailSent.icVerifyNewPasswordEmailSent(dataMap2.get("accountCreation++"),test1,rowNumber);
+                break;
+            case "icResetForgottenPassword":
+                ResetPasswordLink.clickLinkOnGmail(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
+                ResetPasswordLink.resetNewPassword(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
+                ResetPasswordLink.clickUsedResetLink(dataMap2.get(currentKeyWord+"++"),test1,rowNumber);
                 break;
             case "Logout":
                 ic.logout(test1);
@@ -221,6 +266,13 @@ public class JDGTest_TestNG<moduleName> {
                 break;
             case "ProductSearch":
                 products.ic_SelectProductAndAddToCart(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "ClearCart":
+                icCart.removeAllItemsInCart(test1);
+                break;
+            case "CompareProducts":
+                productsCompared.validateAddedProductsCompare(test1,el );
+                productsCompared.clearAllProduct(test1, el);
                 break;
             /*
              * case "iCcartVerification": icCart.iCcartVerification(test1); break;
@@ -353,6 +405,7 @@ public class JDGTest_TestNG<moduleName> {
                 CreditApp_NavigateFilter.VerifyCreditAppSelection(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
                 break;
             case "CreditStatusVerification":
+                CreditStatusVerification.VerifyCreditAppStatus(dataMap2.get("CreditStatusVerification++"), test1, rowNumber);
                 CreditStatusVerification.VerifyCreditAppStatus(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
                 break;
             case "icLogoHomepage":
@@ -375,6 +428,50 @@ public class JDGTest_TestNG<moduleName> {
                 break;
             case "IC_ProductsSortBy":
                 productsSortBy.sortBy(test1);
+                break;
+            case "ic_NavigetoWishlist":
+                NavigetoWishlist.NavigateToWishlist_verifymsg(test1);
+                break;
+            case "ic_verifyWishlistItem":
+                verifyWishlistItem.handleWishlistItem(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "ic_RemoveFromcart":
+                RemoveFromcart.Clear_miniCart(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "ic_verifyLogin":
+                ic_verifyLogin.IC_verifyLogin_addingProductTowishlist(test1, rowNumber);
+                break;
+            case "IC_WishlistToCart":
+                IC_WishlistToCart.verifyProducts_wishlistTocart(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "IncreaseQuanityInCart":
+                increQuantity.increaseQuantity(test1);
+                break;
+            case "RemoveArticleFromCart":
+                removeItemsFromCart.removeItemFromCart(test1);
+            case "SendWishlistToEmail":
+                SendWishlistToEmail.ShareYourwishlist(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "icEmailWishlistverification":
+                icEmailWishlistverification.icWishlistVerificationSender(dataMap2.get(currentKeyWord+"++"), test1, rowNumber);
+                break;
+            case "RedirectToProdDetailPageFromCart":
+                redirectAndVerify.verifyNavigationToProductDetailPageFromCart(test1);
+                break;
+            case "giftCardWithInvalidCouponCode":
+                icRedeemGiftCard.giftCardWithInvalidCouponCode(test1);
+                break;
+            case "ic_logout":
+                ic_login.logout(test1,dataMap2.get("ic_login++"),rowNumber);
+                break;
+            case"verifyCart":
+                icCart.verifyCart(test1);
+                break;
+            case "Pagination":
+                pagination.paginate(test1);
+                break;
+            case"verifyDeliveryOption":
+                icDeliveryOptionDisplay.validateDeliveryOptionsDisplays(test1, rowNumber);
                 break;
         }
     }
