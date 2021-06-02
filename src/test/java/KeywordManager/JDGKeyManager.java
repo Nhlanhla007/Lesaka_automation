c31232df2744cc9059aeb948488913a6e3108417
41066 <41066@BLRTRIDWN41066.ITCINFOTECH.com>
2021-06-02 18:48:20 +0530

    Implemented Parallel Login

--- src/test/java/KeywordManager/JDGKeyManager.java
+++ src/test/java/KeywordManager/JDGKeyManager.java
@@ -130,6 +130,7 @@ public class JDGKeyManager {
         EVS_Cart evs_cart = new EVS_Cart(driver, dataTable2) ;
         EVS_IncreaseQuanityInCart evs_increaseQuantityInCart = new EVS_IncreaseQuanityInCart(driver, dataTable2);
         EVS_RemoveItemsFromCart evs_RemoveItemsFromCart = new EVS_RemoveItemsFromCart(driver, dataTable2);
+        EVS_Parallel_login parallel_evs_Login = new EVS_Parallel_login(driver, dataTable2);
         ExtentTest test1 = test.createNode(moduleToRun);
         int rowNumber = -1;
         if (dataMap2.containsKey(moduleToRun + "++")) {
@@ -462,6 +463,9 @@ public class JDGKeyManager {
             case "evs_RemoveItemsFromCart":
             	evs_RemoveItemsFromCart.removeItemFromCart(test1);
                     break;
+            case "parallel_evs_Login":
+    			parallel_evs_Login.checkParallelExecution(dataMap2.get(moduleToRun + "++"), test1, rowNumber);
+    			break;
 
 
         }
