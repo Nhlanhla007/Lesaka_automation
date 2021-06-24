844638d7a6e3e2f17080dedc9e435c9d3420c95f
37605 <37605@SAJHNELGWN37605.ITCINFOTECH.COM>
2021-06-24 20:24:14 +0530

    update redeem class

--- src/test/java/evs_PageObjects/EVS_RedeemGiftCard.java
+++ src/test/java/evs_PageObjects/EVS_RedeemGiftCard.java
@@ -21,153 +21,147 @@ import utils.DataTable2;
 public class EVS_RedeemGiftCard {
 	
 	WebDriver driver;
-	Action action;
+    Action action;
 	DataTable2 dataTable2;
-	EVS_Cart cart;
+	EVS_Cart cart ;
     
-	public EVS_RedeemGiftCard(WebDriver driver, DataTable2 dataTable2) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-		action = new Action(driver);
-		this.dataTable2 = dataTable2;
-		// ic_products = new Ic_Products(driver, dataTable2);
-		cart = new EVS_Cart(driver, dataTable2);
-	}
+   //Ic_Products ic_products;
 	
-	static Logger logger = Log.getLogData(Action.class.getSimpleName());
+	 public EVS_RedeemGiftCard(WebDriver driver, DataTable2 dataTable2) {
+		 this.driver = driver;
+	        PageFactory.initElements(driver, this);
+	        action = new Action(driver);
+		 this.dataTable2=dataTable2;
+	 //       ic_products = new Ic_Products(driver, dataTable2);
+	       cart = new EVS_Cart(driver, dataTable2);
+	 }
 	 
-	@FindBy(xpath = "//span[contains(text(),'Products')]")
-	WebElement evs_ProductLink;
+	 static Logger logger = Log.getLogData(Action.class.getSimpleName());
 	 
-	@FindBy(css = "a.go-back")
-	WebElement evs_Back;
+	 @FindBy(xpath = "//span[contains(text(),'Products')]")
+	 WebElement evs_ProductLink;
 	 
-	@FindBy(xpath = "//span[contains(text(),'Want to redeem a gift card?')]")
-	WebElement evs_RedeemGiftCardSelect;
+	 //@FindBy(xpath = "//html/body/div[1]/header/div/div/a/span[2]")
+	 @FindBy(css = "a.go-back")
+	 WebElement evs_Back;
 	 
-	@FindBy(xpath = "//input[@id='giftcard-code']")
-	WebElement evs_GiftCardCode;
+	 @FindBy(xpath = "//*[@id=\"block-giftcard-heading\"]")
+	 WebElement evs_RedeemGiftCardSelect;
 	 
-	@FindBy(xpath = "//input[@id='giftcard-scratch-code']")
-	WebElement evs_GiftCardScratchCode;
+	 @FindBy(xpath = "//*[@id=\"giftcard-code\"]")
+	 WebElement evs_GiftCardCode;
 	 
-	@FindBy(xpath = "//span[contains(text(),'Check status')]")
-	WebElement evs_Checkstatus;
+	 @FindBy(xpath = "//*[@id=\"giftcard-scratch-code\"]")
+	 WebElement evs_GiftCardScratchCode;
 	 
-	@FindBy(xpath = "//span[contains(text(),'Current Balance:')]/../span[@class='price']")
-	WebElement card_currentBalance;
+	 @FindBy(xpath = "//*[@id=\"giftcard-form\"]/div/div[3]/div[2]/button/span")
+	 WebElement evs_Checkstatusbalance;
 	 
-	@FindBy(xpath = "//span[contains(text(),'Check status')]/../../../preceding-sibling::div/button")
-	WebElement evs_Apply;
+	 @FindBy(xpath = "//*[@id=\"giftcard-form\"]/div/div[3]/div[1]/button/span")
+	 WebElement evs_Apply;
 	 
 	 @FindBy(xpath = "//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div")
 	 WebElement evs_SuccessfullyApplied;
-
-	@FindBy(xpath = "//*[@id=\"cart-totals\"]/div/table/tbody/tr[1]/td/span")
-	WebElement evs_miniCartSubtotal;
 	 
-	@FindBy(xpath = "//*[@id=\"cart-totals\"]/div/table/tbody/tr[3]/td")
-	WebElement evs_GiftcardAmount;
+	 @FindBy(xpath = "//*[@id=\"cart-totals\"]/div/table/tbody/tr[1]/td/span")
+	 WebElement evs_miniCartSubtotal;
+	 
+	 @FindBy(xpath = "//*[@id=\"cart-totals\"]/div/table/tbody/tr[3]/td")
+	 WebElement evs_GiftcardAmount;
 	 
 	 @FindBy(xpath = "//*[@id=\"cart-totals\"]/div/table/tbody/tr[5]/td/strong/span")
 	 WebElement evs_orderNumber;
 	 				//*[@id="maincontent"]/div[3]/div/div[4]/div[1]/div[1]/ul/li/button/span
 	 @FindBy(xpath="//*[@id=\"maincontent\"]/div[3]/div/div[4]/div[1]/div[1]/ul/li/button/span")
 	    private WebElement evs_Secure;
-
-	@FindBy(xpath = "//*[@id=\"checkout-shipping-method-load\"]/table/tbody/tr[1]/td[4]/button")
-	private WebElement evs_Deliver;
 	 
-	@FindBy(xpath = "//div[contains(text(),'Could not add gift card code')]")
-	private WebElement evs_GiftCardError;
+	 @FindBy(xpath="//*[@id=\"checkout-shipping-method-load\"]/table/tbody/tr[1]/td[4]/button")
+	    private WebElement evs_Deliver;
 	 
-	@FindBy(xpath = "//span[contains(text(),'Order Summary')]/../div/button")
-	private WebElement evs_ContinuePayment;
+	 ///html/body/div[1]/header/div[3]/div[2]/div/div/div
+	 ///html/body/div[1]/header/div[3]/div[2]/div/div/div/text()
+	//div[contains(text(),'Please correct the gift card code.')]
+	 @FindBy(xpath="//div[contains(text(),'Please correct the gift card code.')]")
+	    private WebElement evs_GiftCardError;
 	 
-	@FindBy(xpath = "//*[@class=\"message-error error message\"]")
-	private WebElement invalidCouponCodeErrorPopUp;
-
-	@FindBy(xpath = "//tr[@class='grand totals']/td/strong/span")
-	WebElement evs_totalOrderAmount;
+	 @FindBy(xpath="//*[@id=\"opc-sidebar\"]/div[1]/div[1]/button/span")
+	    private WebElement evs_ContinuePayment;
 	 
-	int finalAmount = 0;
-	int finalOrder = 0;
+	 @FindBy(xpath = "//*[@class=\"message-error error message\"]")
+	 private WebElement invalidCouponCodeErrorPopUp;
+	 
+	 int finalAmount = 0;
+	 int finalOrder = 0;
+	 public void redeemGiftCard(ExtentTest test) throws IOException, Exception{
+		 String giftCardCode = dataTable2.getValueOnCurrentModule("giftCardCode");//input.get("giftCardCode").get(rowNumber);
+		 String scratchCode = dataTable2.getValueOnCurrentModule("scratchCode");//input.get("scratchCode").get(rowNumber);
+		 String UsageType = dataTable2.getValueOnCurrentModule("UsageType");
 		 
-	public void redeemGiftCard(ExtentTest test) throws IOException, Exception {
-		String giftCardCode = dataTable2.getValueOnCurrentModule("giftCardCode");
-		String scratchCode = dataTable2.getValueOnCurrentModule("scratchCode");
-		String UsageType = dataTable2.getValueOnCurrentModule("UsageType");
-			 
-		action.explicitWait(15000);
-		if (action.waitUntilElementIsDisplayed(evs_ContinuePayment, 15000)) {
-			action.click(evs_ContinuePayment, "Click on Continue", test);
-			action.explicitWait(20000);
-			if (action.waitUntilElementIsDisplayed(evs_RedeemGiftCardSelect, 15000)) {
-				action.waitForElementClickable(evs_RedeemGiftCardSelect, "Want to redeem a gift card?", 10);
-				action.scrollElemetnToCenterOfView(evs_RedeemGiftCardSelect, "Redeem Gift Card Tab", test);
-				action.explicitWait(5000);
-				action.click(evs_RedeemGiftCardSelect, "Want to redeem a gift card?", test);
-
-				if (UsageType.equalsIgnoreCase("Redeem")) {
-					action.writeText(evs_GiftCardCode, giftCardCode, "Gift Card code", test);
-					action.writeText(evs_GiftCardScratchCode, scratchCode, "Scratch Code", test);
-					action.click(evs_Apply, "apply the the gift card", test);
-					action.explicitWait(5000);
-					String giftCarddValidate = null;
-					if (action.elementExistWelcome(evs_SuccessfullyApplied, 5, "Gift card added", test)) {
-						giftCarddValidate = action.getText(evs_SuccessfullyApplied, "gift card added", test);
-					}
-
-					action.CompareResult("Gift card added", "Gift Card \"" + giftCardCode.trim() + "\" was added.",giftCarddValidate, test);
-					String subTotal = action.getText(evs_miniCartSubtotal, "Subtotal", test);
-					String cardAmount = action.getText(evs_GiftcardAmount, "CardAmount", test);
-					String finalOrder = action.getText(evs_totalOrderAmount, "value", test);
-
-					finalAmount = (Integer.parseInt(subTotal.replace("R", ""))- Integer.parseInt(cardAmount.replace("-", "").replace("R", "")));
-
-					String s = String.valueOf(finalAmount);
-
-					action.CompareResult("Final order finalized ", "R" + s, finalOrder, test);
-
-					action.click(evs_Secure, "Checkout Secure clicked", test);
-
-				} else if (UsageType.equalsIgnoreCase("Reuse")) {
-
-					action.scrollElemetnToCenterOfView(evs_totalOrderAmount, "Total amount", test);
-					String before_totalOrderPrize = action.getText(evs_totalOrderAmount, "Total Amount before applying gift card", test);
-					action.scrollElemetnToCenterOfView(evs_GiftCardCode, "Gift card code", test);
-					action.writeText(evs_GiftCardCode, giftCardCode, "Gift Card code", test);
-					action.clickEle(evs_Checkstatus, "Check balance", test);
-					action.explicitWait(6000);
-
-					String cardBalance = action.getText(card_currentBalance, "Gift Card Current Balance", test);
-					action.CompareResult("Check gift card Balance", "R" + "0", cardBalance, test);
-					action.click(evs_Apply, "apply the the gift card", test);
-					action.explicitWait(6000);
-					action.waitForElementVisibility(evs_GiftCardError, "Gift card error", 5);				
-					String giftCarddValidate = null;
-					action.scrollElemetnToCenterOfView(evs_GiftCardError, "gift card error", test);
-					giftCarddValidate = action.getText(evs_GiftCardError, "Gift card error",test);
-					action.CompareResult("Incorrect gift Code", "Could not add gift card code", giftCarddValidate,test);
-					action.scrollElemetnToCenterOfView(evs_totalOrderAmount, "Order Amount after gift card applied",test);
-					String after_totalOrderPrize = action.getText(evs_totalOrderAmount, "value", test);
-					action.CompareResult("Validate Updated Total order amount", after_totalOrderPrize,before_totalOrderPrize, test);
-
-				}
+			boolean buttonAvail = action.waitUntilElementIsDisplayed(evs_Back, 15000);
+			action.explicitWait(4000);
+			if (buttonAvail) {
+				evs_Back.click();
 			}
-		}
+			 
+			if (action.waitUntilElementIsDisplayed(evs_RedeemGiftCardSelect, 10000)) {
+				action.scrollElemetnToCenterOfView(evs_RedeemGiftCardSelect, "Redeem Gift Card Tab", test);
+				action.click(evs_RedeemGiftCardSelect, "Before you enter the gift card details", test);
+			}
 
-	}
+			if (UsageType.equalsIgnoreCase("Redeem")) {
+				action.writeText(evs_GiftCardCode, giftCardCode, "Gift Card code", test);
+				action.writeText(evs_GiftCardScratchCode, scratchCode, "Scratch Code", test);
+				// click apply
+				action.click(evs_Apply, "apply the the gift card", test);
+				String giftCarddValidate = null;
+				if (action.elementExistWelcome(evs_SuccessfullyApplied, 4, "Gift card added", test)) {
+					giftCarddValidate = action.getText(evs_SuccessfullyApplied, "gift card added", test);
+				}
 
-	public void giftCardWithInvalidCouponCode(ExtentTest test) throws Exception {
-		action.click(evs_RedeemGiftCardSelect, "Redeem gift cart", test);
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,1000)");
-		action.writeText(evs_GiftCardCode, "abcdGiftWrongGiftCart", "Enter incorrect gift cart code", test);
-		action.click(evs_Apply, "Apply gift card coupon code", test);
-		action.elementExistWelcome(invalidCouponCodeErrorPopUp, 7, "Invalid Coupon Code pop up error", test);
-		String popUpErrorMessage = invalidCouponCodeErrorPopUp.findElement(By.xpath(".//div")).getText();
-		action.CompareResult("Pop up error message", "Please correct the gift card code.", popUpErrorMessage, test);
-	}
+				action.CompareResult("Gift card added", "Gift Card \"" + giftCardCode.trim() + "\" was added.",
+						giftCarddValidate, test);
+				String subTotal = action.getText(evs_miniCartSubtotal, "Subtotal", test);
+				String cardAmount = action.getText(evs_GiftcardAmount, "CardAmount", test);
+				String finalOrder = action.getText(evs_orderNumber, "value", test);
+
+				finalAmount = (Integer.parseInt(subTotal.replace("R", ""))
+						- Integer.parseInt(cardAmount.replace("-", "").replace("R", "")));
+
+				String s = String.valueOf(finalAmount);
+
+				action.CompareResult("Final order finalized ", "R" + s, finalOrder, test);
+
+				 //validate
+		 action.click(evs_Secure, "Checkout Secure clicked", test);
+		 
+		 }else if(UsageType.equalsIgnoreCase("Reuse")){
+			 action.scrollElemetnToCenterOfView(evs_GiftCardCode, "Gift card code", test);
+			 action.writeText(evs_GiftCardCode, giftCardCode, "Gift Card code", test);
+			 action.writeText(evs_GiftCardScratchCode, scratchCode, "Scratch Code", test);
+			 
+			 action.click(evs_Apply, "apply the the gift card", test);
+			 String giftCarddValidate = null;
+			 if(action.elementExistWelcome(evs_GiftCardError, 4, "Please correct the gift card code.", test)){
+				 giftCarddValidate = action.getText(evs_GiftCardError, "Please correct the gift card code.",test);				 
+				 action.explicitWait(5000);
+			 }
+			 
+		 }
+		 
+	 }
+
+	 
+	 public void giftCardWithInvalidCouponCode(ExtentTest test) throws Exception {
+	//	 cart.navigateToCart(test);
+		// cart.navigateToViewAndEditCart(test);
+		 action.click(evs_RedeemGiftCardSelect, "Redeem gift cart", test);
+		 JavascriptExecutor js = (JavascriptExecutor) driver;
+		 js.executeScript("window.scrollBy(0,1000)");
+		 action.writeText(evs_GiftCardCode, "abcdGiftWrongGiftCart", "Enter incorrect gift cart code", test);
+		 action.click(evs_Apply, "Apply gift card coupon code", test);
+		 action.elementExistWelcome(invalidCouponCodeErrorPopUp, 7, "Invalid Coupon Code pop up error", test);
+		 String popUpErrorMessage = invalidCouponCodeErrorPopUp.findElement(By.xpath(".//div")).getText();
+		 action.CompareResult("Pop up error message", "Please correct the gift card code.", popUpErrorMessage, test);
+	 }
 	 
 }
