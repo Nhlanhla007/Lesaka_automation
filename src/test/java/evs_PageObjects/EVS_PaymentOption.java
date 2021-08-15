8a113f46410d12527abd82b60d11ec6fb9047a5c
41066 <praveen.ojha@itcinfotech.com>
2021-08-15 11:26:57 +0530

    Updated _PaymentOption.java v4

--- src/test/java/evs_PageObjects/EVS_PaymentOption.java
+++ src/test/java/evs_PageObjects/EVS_PaymentOption.java
@@ -133,9 +133,7 @@ public class EVS_PaymentOption {
                     action.CompareResult("No Document is selected for Upload", "true", String.valueOf(NoFileSelectionCheck), test);
                     if (NoFileSelectionCheck) {
 
-//                        String filePath= JavaUtils.getAbsoluteFilePath("./Reports/ID_&_Passport.png");
-
-                        String filePath = System.getProperty("user.dir") + "//src//test//resources//ID_&_Passport.png";
+                        String filePath = System.getProperty("user.dir") + "./src/test/resources/ID_&_Passport.png";
                         selectIDButton.sendKeys(filePath);
                         action.ajaxWait(10,test);
                         boolean uploadMessage = action.waitUntilElementIsDisplayed(uploadMsg, 5);
