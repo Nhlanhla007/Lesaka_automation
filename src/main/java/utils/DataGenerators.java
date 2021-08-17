2459062cba5589ff311b2f49284642b4ac87cef4
37605 <37605@SAJHNELGWN37605.ITCINFOTECH.COM>
2021-08-17 15:08:51 +0530

    Passport generate keyword has been added

--- src/main/java/utils/DataGenerators.java
+++ src/main/java/utils/DataGenerators.java
@@ -101,6 +101,13 @@ public class DataGenerators {
         idNumber = fakeValuesService.regexify("(((\\d{2}((0[13578]|1[02])(0[1-9]|[13]\\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\\d|30)|02(0[1-9]|1\\d|2[0-8])))|([02468][048]|[13579][26])0229))((\\d{4})(\\d{3})|(\\d{7}))");
         return idNumber;
     }
+    
+    
+    private String GenerateValidPassPortNumber() {
+        String passportNumber;
+        passportNumber = fakeValuesService.regexify("[A-Z]{2}[0-9]{2}[abcd]{2}[0-9]{2}");
+        return passportNumber;
+    }
 
     public String GenerateRequiredData(String dataTypeRequired) {
         String requiredData = null;
@@ -150,6 +157,9 @@ public class DataGenerators {
                 break;
             case "GENERATED VALID SA ID NUMBER":
                 requiredData = GenerateValidSAIdNumber();
+                break;
+            case "GENERATED Passport NUMBER":
+                requiredData = GenerateValidPassPortNumber();
                 break;
             case "GENERATED NUMBER - 10":
                 requiredData = faker.number().digits(10).replace('0', '1');
