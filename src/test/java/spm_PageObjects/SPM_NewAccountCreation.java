82354ee28698293d30b64b0301d0293dbeb4295d
228330 <228330@C10NBIT228330.jdg.co.za>
2022-01-05 13:52:18 +0200

    Cell phone validation mandatory field for SPM TA1304

--- src/test/java/spm_PageObjects/SPM_NewAccountCreation.java
+++ src/test/java/spm_PageObjects/SPM_NewAccountCreation.java
@@ -130,6 +130,8 @@ public class SPM_NewAccountCreation {
     @FindBy(id = "cellphone_number")
     WebElement telephoneNumber;
     
+    @FindBy(xpath = "//div[@id='cellphone_number-error']")
+	  WebElement telephoneErrorM;
   // Sourav TA19 customer info after click on Customer info 
 	
 	  @FindBy(xpath = "//span[contains(text(),'Account Information')]")
@@ -202,7 +204,8 @@ public class SPM_NewAccountCreation {
 		String saIDvalidateIDWithLessDigits = dataTable2.getValueOnCurrentModule("validateIDWithLessDigits");//input.get("validateIDWithLessDigits").get(rowNumber);
 		String saIDvalidateIDWithMoreDigits = dataTable2.getValueOnCurrentModule("validateIDWithMoreDigits");//input.get("validateIDWithMoreDigits").get(rowNumber);
 		String existingAccountValidation =dataTable2.getValueOnCurrentModule("validateExistingAccount");//input.get("validateExistingAccount").get(rowNumber);		
-
+		String cellPhoneNumberValidate = dataTable2.getValueOnCurrentModule("validateCellPhoneNumber");
+		
 //		try {
 			spm_NavigateToCreateAccount(test);
 			
@@ -265,9 +268,14 @@ public class SPM_NewAccountCreation {
 				ic_VerifySAIDLimit(identityWithMore, test);
 
 			}
-
 			
 			
+			  if (cellPhoneNumberValidate.equalsIgnoreCase("yes")) {
+			  System.out.println("Enters validate with more digits");
+			  telephoneNumber.clear();
+			  
+			  }
+			 
 			if(passwordValidation.equalsIgnoreCase("yes")) {
 				confirmPassword = ic_VerifyPasswordcanDiffer(confirmPassword);
 				action.writeText(User_ConfirmPassword, confirmPassword, "Confirm password", test);
@@ -287,9 +295,16 @@ public class SPM_NewAccountCreation {
 			
 			if(!(saIDvalidateIncorrectID.equalsIgnoreCase("yes") | saIDvalidateIDWithLessDigits.equalsIgnoreCase("yes") | 
 					saIDvalidateIDWithMoreDigits.equalsIgnoreCase("yes")  | 
-					passwordValidation.equalsIgnoreCase("yes") | existingAccountValidation.equalsIgnoreCase("yes"))) {
+					passwordValidation.equalsIgnoreCase("yes") | existingAccountValidation.equalsIgnoreCase("yes") | cellPhoneNumberValidate.equalsIgnoreCase("yes"))) {
 				Verify_Acount_Information(test, firstName, lastName, emailAddress, identityNumber,identityType,selectNewsLetter);//,taxVatNumbe,tavVatNumberFlagStatus
 			}
+			
+			if (action.elementExistWelcome(telephoneErrorM, 4, "Check CellPhone mandatory field", test)) {
+					  telephoneNumber.clear();
+					  action.scrollElemetnToCenterOfView(telephoneErrorM, "To Check For Error Message", test);
+					  action.CompareResult("The Cellphone Number Is A Required Field", "This is a required field.", telephoneErrorM.getText(), test);
+			}
+				  
 			/*
 			 * if(verifyMagentoDetails.equalsIgnoreCase("Yes")) {
 			 * Magento_VerifyCustomerDetails(test
